1abfcacf6af5a626fa9173a1c4f2302dedb6c809
ArashiDesu <47567561+ArunSriKrishna@users.noreply.github.com>
2020-11-27 21:03:08 +0530

    Update Quiz.java

--- QuizGUI/src/Class/Quiz.java
+++ QuizGUI/src/Class/Quiz.java
@@ -1,20 +1,16 @@
 package Class;
 
 
-
-
-//Class Quiz holds global variables related to Quiz TODO: Remove Static keywords
-
 public class Quiz
 {
     //for "class Quiz", holds the values that like global variaable and do not change for the a quix  
-    public static String Quiz_Name = "";
+    public static String Quiz_Name = "null";
     
     //last_count holds the total number of question in the quiz
     public static int last_count = 0;
     
     //holds total time allocated to the quiz in seconds
-    public static int total_time = 100;
+    public static int total_time = 0;
     
     //Time left is calcualted in each question form as time_left - time_taken( refer below )
     public static int time_left = 0;
@@ -25,9 +21,6 @@ public class Quiz
     //Holds total score possible in the quiz
     public static int score_max = 0;
     
-    //state_review is boolean that stores the state whether the user is review the question (after the result), while reviewing the correct answer is shown, default its false, after result is shown it is set to true
-    public boolean state_review = false;
-    
     //store the current position (question) in the quiz
-    public static int current_count = 0;
+    public int current_count = 0;
 }
