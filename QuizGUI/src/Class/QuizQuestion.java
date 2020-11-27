ee361aae0d62353d2044460daa8e2fade9f42fd0
ArashiDesu <47567561+ArunSriKrishna@users.noreply.github.com>
2020-11-27 21:02:32 +0530

    Update QuizQuestion.java

--- QuizGUI/src/Class/QuizQuestion.java
+++ QuizGUI/src/Class/QuizQuestion.java
@@ -1,10 +1,6 @@
 package Class;
 
 
-
-//This is test class simulating the quiz class, TODO: remove keyword static
-//QuizQuestion hold local variables for the specific question
-
 public class QuizQuestion 
 {   
     //Holds question_type out of possible values = {single-word, true-false, number, single-option, multiple-option}
@@ -17,43 +13,26 @@ public class QuizQuestion
     public int question_marks = 0;
     
     //Holds question string and options
-    public String question_string = "";
-    public String options[] = {"", "", "", ""};
+    public String question_string = "null";
+    public String options[] = {"null", "null", "null", "null"};
   
-    //Holds the values inputted by the user (Note all values are intialized to null or -1
+    //Holds the answers inputted by the user 
     public String answer_string = null;
     
-    //Holds the number value inputted by the user
-    public float answer_number = -1;
-    
-    //Holds the value selected by the user for single answer mcq
-    public int answer_option = -1;
-    
-    //Holds the value selected by the user for true or false question
+    public Float answer_number = null;
+
     public Boolean answer_tf = null;
     
-    //Holds the values selected by the user for multiple answer mcq
-    public int answer_option1 = -1;
-    public int answer_option2 = -1;
-    public int answer_option3 = -1;
-    public int answer_option4 = -1; 
+    public int[] answer_options = new int[]{-1, -1, -1, -1};
+ 
+    // Holds the correct answers for the question     
+    public String correct_answer_string = null;
     
-    // Holds the actual correct answer for the question     
-    public String correct_answer_string = "";
-    public int correct_answer_number = 0;
-    
-    //In case of single option
-    public int correct_answer_option = -1;
-    
-    //In case of true or false question
-    public Boolean correct_answer = null;
-    
-    //In case of multiple correct option
-    public int correct_answer_option1 = -1;
-    public int correct_answer_option2 = -1;
-    public int correct_answer_option3 = -1;
-    public int correct_answer_option4 = -1;  
-    
-    //Time taken to answer the question, used to calculate the time left in each "next" or "previous" button clicks, its calculated as time_left = time_left - time_taken;
+    public Float correct_answer_number = null;
+        
+    public Boolean correct_tf = null;
+
+    public int[] correct_answer_options = new int[4];
+
     public static int time_taken;
 }
