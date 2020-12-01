019d184a1d5c14625f480b5099cab5df12a4afb9
ArashiDesu <47567561+ArunSriKrishna@users.noreply.github.com>
2020-12-01 19:20:51 +0530

    fized bugs

--- QuizGUI/src/Class/Evaluate.java
+++ QuizGUI/src/Class/Evaluate.java
@@ -1,66 +1,60 @@
 
 package Class;
+
+import java.util.Objects;
+
 public class Evaluate {
 
-    public void Evaluate_Quiz(Quiz q1, QuizQuestion q2)
+    public void Evaluate_Quiz(QuizQuestion question)
     {
      
-        if(q2.question_type.equals("single-word"))
-        {
-            if(q2.answer_string.equals(q2.correct_answer_string))
-            {
-                q1.score += q2.question_marks;
-            }
-        }
-        else if(q2.question_type.equals("true-false"))
-        {
-            if(q2.answer_tf == q2.correct_tf)
-            {
-                q1.score += q2.question_marks;
-            }
-        }
-        else if(q2.question_type.equals("number"))
-        {
-            if(q2.answer_number == q2.correct_answer_number)
-            {
-                q1.score += q2.question_marks;
-            }
-            
-        }
-        else if(q2.question_type.equals("single-option"))
-        {
-            Boolean correct = true;
-            for(int i =0; i<3; i++)
-            {
-                if(q2.answer_options[i] != q2.correct_answer_options[i])
+        switch (question.question_type) {
+            case "single-word":
+                if(question.answer_string.equalsIgnoreCase(question.correct_answer_string))
                 {
-                    correct = false;
-                }
-            }
-            
-            if(correct == true)
-            {
-                q1.score += q2.question_marks;
-            }
-            
-        }
-        else if(q2.question_type.equals("multiple-option"))
-        {
-            Boolean correct = true;
-            for(int i =0; i<3; i++)
-            {
-                if(q2.answer_options[i] != q2.correct_answer_options[i])
+                    Quiz.score += question.question_marks;
+                }   break;
+            case "true-false":
+                if(Objects.equals(question.answer_tf, question.correct_tf))
                 {
-                    correct = false;
+                    Quiz.score += question.question_marks;
+                }   break;
+            case "number":
+                if(Objects.equals(question.answer_number, question.correct_answer_number))
+                {
+                    Quiz.score += question.question_marks;
+                }   break;
+            case "single-option":
+                {
+                    Boolean correct = true;
+                    for(int i =0; i<3; i++)
+                    {
+                        if(question.answer_options[i] != question.correct_answer_options[i])
+                        {
+                            correct = false;
+                        }
+                    }       if(correct == true)
+                    {
+                        Quiz.score += question.question_marks;
+                    }       break;
                 }
-            }
-            
-            if(correct == true)
-            {
-                q1.score += q2.question_marks;
-            }
-            
-            
+            case "multiple-option":
+                {
+                    Boolean correct = true;
+                    for(int i =0; i<3; i++)
+                    {
+                        if(question.answer_options[i] != question.correct_answer_options[i])
+                        {
+                            correct = false;
+                        }
+                    }       if(correct == true)
+                    {
+                        Quiz.score += question.question_marks;
+                    }       break;
+                }
+            default:
+                
+                break;
         }
         
     }
