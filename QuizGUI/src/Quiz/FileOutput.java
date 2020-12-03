d075fbd2db2b074a6124be4563df8e5febb8444a
ArashiDesu <47567561+ArunSriKrishna@users.noreply.github.com>
2020-12-03 15:41:03 +0530

    added vinay's part

--- QuizGUI/src/Quiz/FileOutput.java
+++ QuizGUI/src/Quiz/FileOutput.java
@@ -3,7 +3,50 @@ package Quiz;
 /**
  *
  * @author Vinay M
- */
-public class FileOutput {
+ */ 
+
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+public class FileOutput
+{
+  public static void main(String[] args) 
+  {
     
+    try 
+    {
+        File myObj = new File("result.txt");
+
+        if (myObj.createNewFile()) 
+        {
+
+          System.out.println("Name of the Quiz: " + Quiz.Quiz_Name);
+
+          System.out.println("Total Number of Question: " + Quiz.last_count);
+
+          System.out.println("Time Allocated: " + Quiz.total_time);
+
+          System.out.println("Time Taken: " + Quiz.time_taken);
+
+          System.out.println("Maximum Score: " + Quiz.score_max);
+
+          System.out.println("Score Obtained: " + Quiz.score);
+
+          System.out.println("Total Question Attempted: " + Quiz.current_count+1);
+
+        } 
+
+        else 
+        {    
+          System.out.println("File already exists.");
+        }
+    } 
+      
+    catch (IOException e) 
+    {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    
+  }
 }
